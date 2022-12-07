843db207ec7ba010302b2beafb8d2c431f5542b3
MintarasGrinius <74651345+MintarasGrinius@users.noreply.github.com>
2022-12-07 19:58:56 +0200

    General modal

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,6 +4,7 @@ import { requireUserId } from '~/utils/auth.server'
 import { Layout } from '~/components/layout'
 import { UserPanel } from '~/components/user-panel'
 import { getOtherUsers } from '~/utils/user.server'
+import { useLoaderData, Outlet } from '@remix-run/react'
 
 export const loader: LoaderFunction = async ({ request }) => {
   const userId = await requireUserId(request)
@@ -12,10 +13,14 @@ export const loader: LoaderFunction = async ({ request }) => {
 }
 
 export default function Home() {
+  const { users } = useLoaderData()
+
   return (
     <Layout>
+      <Outlet />
       <div className='h-full flex'>
-        <UserPanel />
+        <UserPanel users={users} />
+        <div className='flex-1'></div>
       </div>
     </Layout>
   )
---
+++ app/routes/home/kudo.$userId.tsx
@@ -0,0 +1,21 @@
+import type { LoaderFunction } from '@remix-run/node'
+import { redirect } from '@remix-run/node'
+import { json } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+import { getUserById } from '~/utils/user.server'
+
+export const loader: LoaderFunction = async ({ request, params }) => {
+  const { userId } = params
+
+  if (typeof userId !== 'string') {
+    return redirect('/home')
+  }
+
+  const recipient = await getUserById(userId)
+  return json({ recipient })
+}
+
+export default function KudoModal() {
+  const data = useLoaderData()
+  return <h2> User: {data.userId} </h2>
+}
